97a2355f3d1b61b5821437aeb61d0d5d106ac8d9
Antonio Nikolov <antonionikolov93@gmail.com>
2026-01-20 17:41:01 +0200

    Initialization

---
+++ src/main/java/com/tgroup/fast_score/FastScoreApplication.java
@@ -0,0 +1,13 @@
+package com.tgroup.fast_score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastScoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgroup/fast_score/FastScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgroup.fast_score;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
