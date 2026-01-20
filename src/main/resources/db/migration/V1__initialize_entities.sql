b4597e0d68312c065c80f115303c6a251879c6bb
Antonio Nikolov <antonionikolov93@gmail.com>
2026-01-20 23:54:51 +0200

    Adds initial schema

---
+++ src/main/resources/db/migration/V1__initialize_entities.sql
@@ -0,0 +1,78 @@
+drop table if exists players;
+drop table if exists participants;
+drop table if exists tournaments;
+drop table if exists stages;
+drop table if exists matches;
+drop table if exists tournament_contacts;
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    is_account_non_locked BOOLEAN DEFAULT TRUE,
+    last_login TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE players (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    display_name VARCHAR(100) NOT NULL,
+    user_id UUID REFERENCES users(id) ON DELETE SET NULL,
+    is_claimed BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE tournaments (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(60) NOT NULL,
+    starts_at TIMESTAMP NOT NULL, -- Fixed name and type
+    venue_id UUID,
+    venue_name_manual VARCHAR(100),
+    organiser_type VARCHAR(20) NOT NULL,
+    organiser_id UUID NOT NULL,
+    format VARCHAR(50),
+    race_to SMALLINT,
+    handicap BOOLEAN DEFAULT FALSE,
+    status VARCHAR(20) DEFAULT 'DRAFT'
+);
+
+CREATE TABLE tournament_contacts (
+    tournament_id UUID REFERENCES tournaments(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(id),
+    role VARCHAR(30),
+    PRIMARY KEY (tournament_id, user_id)
+);
+
+CREATE TABLE participants (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    tournament_id UUID NOT NULL REFERENCES tournaments(id) ON DELETE CASCADE,
+    player_id UUID NOT NULL REFERENCES players(id),
+    rank_at_registration VARCHAR(10),
+    seed INTEGER,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(tournament_id, player_id)
+);
+
+CREATE TABLE matches (
+    id UUID PRIMARY KEY NOT NULL,
+    tournament_id UUID NOT NULL REFERENCES tournaments(id) ON DELETE CASCADE,
+    parent_match_id UUID REFERENCES matches(id) ON DELETE CASCADE,
+
+    -- Players
+    participant1_id UUID REFERENCES participants(id),
+    participant2_id UUID REFERENCES participants(id),
+    winner_id UUID REFERENCES participants(id),
+
+    -- Scoring
+    score1 SMALLINT DEFAULT 0,
+    score2 SMALLINT DEFAULT 0,
+
+    -- Metadata for UI/Logic
+    status VARCHAR(20) DEFAULT 'PENDING', -- PENDING, LIVE, COMPLETED, DISPUTED
+    -- the number of the round: quater final, final, etc.
+    round_number SMALLINT NOT NULL,
+    -- how the matches should be sorted in the bracket
+    sort_order SMALLINT,
+
+    -- Progression Logic
+    next_match_id UUID REFERENCES matches(id),
+    next_match_slot INT CHECK (next_match_slot IN (1, 2))
+);
