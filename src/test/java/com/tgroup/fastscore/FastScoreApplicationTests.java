682583730e828fae199a6df11e0a1f693e8855d4
Antonio Nikolov <antonionikolov93@gmail.com>
2026-01-31 18:14:28 +0200

    Fix spring boot test

--- src/test/java/com/tgroup/fastscore/FastScoreApplicationTests.java
+++ src/test/java/com/tgroup/fastscore/FastScoreApplicationTests.java
@@ -1,10 +1,23 @@
 package com.tgroup.fastscore;
 
-//@SpringBootTest
-//@EmbeddedKafka(partitions = 1)
-//class FastScoreApplicationTests {
-//	@Test
-//	void contextLoads() {
-//		// Your test will now wait for Kafka to start in Docker
-//	}
-//}
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@EmbeddedKafka(partitions = 1)
+@Testcontainers
+class FastScoreApplicationTests {
+	@Container
+	@ServiceConnection // This tells Spring to grab the URL/Username/Password from the container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+	@Test
+	void contextLoads() {
+		// Your test will now wait for Kafka to start in Docker
+	}
+}
